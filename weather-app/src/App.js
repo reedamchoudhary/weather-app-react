7525d21ad068c88e6c6c33fc37c9510bb084c37f
Reedam <reedamchoudhary169@gmail.com>
2021-05-25 19:01:51 +0530

    startin project

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,10 +1,11 @@
-const api = {
-  key : "a430887340ef9c6c9519cc272217749d",
-  base : "https://home.openweathermap.org/api_keys"
-}
+import Weather from "./Weather"
 
 const App = () =>{
-  return <h1>Hey, It's a test!</h1>
+  return (
+    <>
+      <Weather />
+    </>
+    )
 }
 
 export default App
